aba065015e25f6b445582320214701a68b424bf3
qhxiv <eknight1412@gmail.com>
2025-11-18 22:20:19 +0700

    update

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,7 +1,9 @@
 return {
-  "sainnhe/gruvbox-material",
-  config = function()
-    vim.g.gruvbox_material_background = "hard"
-    vim.cmd("colo gruvbox-material")
-  end,
+  {
+    "sainnhe/gruvbox-material",
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.cmd("colo gruvbox-material")
+    end,
+  },
 }
