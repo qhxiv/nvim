ae498b1eebe198967bd3930dc0f1f838f5346bd9
qhxiv <eknight1412@gmail.com>
2026-01-05 21:40:04 +0700

    update

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,9 +1,6 @@
 return {
-  {
-    "sainnhe/gruvbox-material",
-    config = function()
-      vim.g.gruvbox_material_background = "hard"
-      vim.cmd("colo gruvbox-material")
-    end,
-  },
+  "Mofiqul/vscode.nvim",
+  config = function()
+    vim.cmd("colo vscode")
+  end
 }
