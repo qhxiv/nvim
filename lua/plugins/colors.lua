bb9e9b6e7e129398eed732e8fbb4be9ef3321b26
qhxiv <eknight1412@gmail.com>
2026-05-29 16:39:37 +0700

    update

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,6 +1,26 @@
 return {
-  "Mofiqul/vscode.nvim",
-  config = function()
-    vim.cmd("colo vscode")
-  end,
+  {
+    "Mofiqul/vscode.nvim",
+    config = function()
+      -- vim.cmd("colo vscode")
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("kanagawa").setup({
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+      })
+
+      vim.cmd("colo kanagawa")
+    end,
+  },
 }
